1eccd31e99d0beac41bc1ea3ad71ed26bff2e874
SIMON RIOS <sprios@uc.cl>
2021-06-12 11:25:23 +0200

    second commit

---
+++ src/com/ironhack/git/main/Git.java
@@ -0,0 +1,10 @@
+package com.ironhack.git.main;
+
+public class Git {
+
+    public static void main(String[] args) {
+    System.out.println("hello git :D");
+        System.out.println("change");
+    }
+
+}

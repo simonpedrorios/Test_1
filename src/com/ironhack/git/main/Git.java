f273d5c1defbce8b7a7493b827aabd2741b615b2
SIMON RIOS <sprios@uc.cl>
2021-06-12 11:41:45 +0200

    new brach

--- src/com/ironhack/git/main/Git.java
+++ src/com/ironhack/git/main/Git.java
@@ -3,7 +3,7 @@ package com.ironhack.git.main;
 public class Git {
 
     public static void main(String[] args) {
-    System.out.println("hello git :D");
+    System.out.println("hello git :D new branch");
         System.out.println("change");
         System.out.println("new");
     }
